0769393513c66e9e1231da01f9a87ce2cb89063c
Ganna VYNOGRADOVA <gvynogra@e3r8p3.unit.ua>
2018-03-08 15:40:27 +0200

    nothing works

--- ft_ls.c
+++ ft_ls.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <time.h>
 #include "libftprintf/includes/libft.h"
+#include <errno.h>
 
 /*
  struct stat { // when _DARWIN_FEATURE_64_BIT_INODE is NOT defined //
@@ -29,6 +30,13 @@
 
      int 		fstat(int fildes, struct stat *buf);
      int 		lstat(const char *restrict path, struct stat *restrict buf);
+
+
+	char list[][FIELDS] = 
+	{
+	"d rw-rw-rw 1 "
+	};
+
 */
 
 typedef	struct s_options
@@ -65,9 +73,14 @@ typedef enum
 	T_END
 } e_time;
 
+void ft_read_args(char *name, t_options *options);
+void ft_print_time(struct stat *buf);
+void ft_read_file();
+void ft_read_dir(DIR *dirp, t_options *options);
+
+
 void ft_print_time(struct stat *buf)
 {
-	//printf("%s\n",__FUNCTION__ );
 	char mtime[100];
 	char **date;
 
@@ -84,49 +97,90 @@ void ft_print_time(struct stat *buf)
 	ft_putchar(' ');
 	ft_putstr(date[T_DAY]);
 	ft_putchar(' ');
-	ft_putstr(date[T_TIME]);
-	ft_putchar('\n');
+	write(1, date[T_TIME], 5);
+	ft_putchar(' ');
+}
+void ft_read_file()
+{
+	;
 }
 
-void ft_read_file(char *name, t_options *options)
+void ft_read_dir(DIR *dirp, t_options *options)
 {
-	/*DIR *dirp;
 	struct dirent *info;
-	
-	dirp = opendir(name);
-	if (!dirp)
-	{
-		fd = open()
-	}
 	while ((info = readdir(dirp)))
+	{
+		if (ft_strequ(info->d_name, ".")||ft_strequ(info->d_name, ".."))
+		 	{
+		 		printf("->	%s\n", info->d_name);
+		 		continue;
+		 	}
+		if (options->R)
+			ft_read_args(info->d_name, options);
+		//else
 		printf("->	%s\n", info->d_name);
-	closedir(dirp);*/
+	}
+}
+
+
+void ft_read_args(char *name, t_options *options)
+{
+	int ret;
+	DIR *dirp;
+	
+	
+	
 	struct stat buf;
-	stat(name, &buf);
-	if (options->l)
-		ft_print_time(&buf);
-	printf("read: %s\n", name);
+
+	ret = stat(name, &buf);
+	if (ret >= 0)
+{
+	if (S_ISREG(buf.st_mode))
+	{
+		ft_putstr("f.	");
+		printf("%s\n", name);
+		ft_read_file();
+	}
+	else if (S_ISDIR(buf.st_mode))
+	{
+		ft_putstr("d.	");
+		//printf("%s\n", name);
+		dirp = opendir(name);
+		ft_read_dir(dirp, options);
+		closedir(dirp);
+	}
+}
+else
+{
+	printf("%s\t", name);
+	perror(strerror(ret));
+}
+	/*if (options->l)
+		ft_print_time(&buf);*/
+	
 }
 
 int	main(int argc, char **argv)
 {
 	int i = 1;
+	//char content[][FIELDS];
+	//int fc = 0;
 
 	t_options *options;
 
 	options = (t_options*)ft_memalloc(sizeof(t_options));
-
+	if (!options)
+		perror("Error");
+printf("-------------------------------------------------------------\n");
 	if (argc > 1)
 	{
 		while (i < argc)
 		{
 			if (*argv[i] == '-')
 			{
-				//printf("--------->'%c'\n", *argv[i]);
 				++argv[i];
 				while (*argv[i] && ft_isalnum(*argv[i]))
 					{
-						//printf("--------->'%c'\n", *argv[i]);
 						if (*argv[i] == 'a')
 							options->a = 1;
 						else if (*argv[i] == 'l')
@@ -142,23 +196,26 @@ int	main(int argc, char **argv)
 							printf("illegal option '%c'\n", *argv[i]);
 							return(1);
 						}
-						//printf("first circle done\n");
 						argv[i]++;
 					}
 					i++;
 			}
 			if (!argv[i])
-				ft_read_file(".", options);
+				ft_read_args(".", options/*, &content*/);
 			else
-				ft_read_file(argv[i], options);
+				{
+					ft_read_args(argv[i], options/*, &content*/);
+					//fc++;
+				}
 			i++;
 		}
 		
 	}
 	else
-		ft_read_file(".", options);
+		ft_read_args(".", options/*, &content*/);
 		
-	printf("\n\n				options {a - %d, l - %d, R - %d, r - %d, t - %d}\n", options->a, options->l, options->R, options->r, options->t);
+	printf("\n-------------------------------------------------------------\noptions {a - %d, l - %d, R - %d, r - %d, t - %d}\n", options->a, options->l, options->R, options->r, options->t);
+	//printf("fc: %d\n", fc);
 	free(options);
 	return 0;
 }
